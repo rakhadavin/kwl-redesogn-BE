3fb2c9f757c2853b2d670bcc02dec9a986fc7bd2
Agnes Audya Tiara P <agnes.audya@ui.ac.id>
2024-04-09 17:41:16 +0700

    add know quiz

--- crudkwl/models.py
+++ crudkwl/models.py
@@ -1,29 +1,37 @@
 from django.db import models
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Know(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE, blank=True, null=True)
+    score = models.IntegerField(default=0)
     def __str__(self):
         return self.title
 
 class WantToKnow(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE, blank=True, null=True)
+    score = models.IntegerField(default=0)
     def __str__(self):
         return self.title
 
 class Learned(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE, blank=True, null=True)
+    score = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+    
+class KnowQuiz(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    question = models.CharField(max_length=255)
+    know_id = models.ForeignKey('Know', on_delete=models.CASCADE, blank=True, null=True)
+    answers = ArrayField(models.CharField(max_length=255), blank=True, null=True)
     def __str__(self):
         return self.title
