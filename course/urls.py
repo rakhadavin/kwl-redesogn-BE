1d26198053137b24a51ff3d2713be7f88b231e12
Agnes Audya Tiara P <agnes.audya@ui.ac.id>
2024-04-22 15:15:05 +0700

    add url for get topic id by course_id

--- course/urls.py
+++ course/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>', views.CourseDetailView.as_view()),
     path('topic/', views.TopicList.as_view()),
     path('topic/<int:pk>', views.TopicDetail.as_view()),
+    path('topic/all/<int:course_id>', views.CourseTopicView.as_view),
 ]
 
