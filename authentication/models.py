4cf7ba186f306b7fa1f758dd2860a79f4a83f54d
Agnes Audya Tiara P <agnes.audya@ui.ac.id>
2024-05-07 23:02:59 +0700

    Analysis

--- authentication/models.py
+++ authentication/models.py
@@ -10,7 +10,6 @@ class KwlUser(AbstractUser):
     domisili = models.CharField(max_length=100, help_text="User's domicile", null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
     reset_password_token = models.CharField(max_length=100, null=True, blank=True, help_text="Token for resetting password")
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
     def __str__(self):
         return self.username
