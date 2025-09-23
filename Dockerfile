3d2a27fbe51e60f60b3c15da6fbd1bbfa64ab5ae
Adam Muhammad <adam.muhammad@cs.ui.ac.id>
2025-09-23 15:19:27 +0700

    feat: Update CI/CD configuration and Docker setup for improved deployment process

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM python:3.10.12-slim-bullseye
+FROM python:3.13-slim-bookworm
 
-WORKDIR /app
-COPY . /app 
+WORKDIR /usr/src/app
+COPY . .
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
 RUN apt-get update && apt-get install -y \
     nano \
@@ -14,11 +17,11 @@ ENV TZ=Asia/Jakarta
 RUN ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime && \
     echo "Asia/Jakarta" > /etc/timezone
 
-RUN python3 manage.py collectstatic --no-input
-RUN python3 manage.py migrate
+# RUN python3 manage.py collectstatic --no-input
+# RUN python3 manage.py migrate
 
 COPY ./start.sh /start.sh
-RUN dos2unix /start.sh
+# RUN dos2unix /start.sh
 RUN chmod +x /start.sh
 
 EXPOSE 8042
--- start.sh
+++ start.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
+set -e
+
+LOGFILE=app_logs/django_startup.log
+
+echo ">> Starting Django setup..." | tee -a $LOGFILE
+
+python manage.py showmigrations | tee -a $LOGFILE
+echo ">> showmigrations done" | tee -a $LOGFILE
+
+python manage.py makemigrations --noinput | tee -a $LOGFILE
+echo ">> makemigrations done" | tee -a $LOGFILE
+
+python manage.py migrate --noinput | tee -a $LOGFILE
+echo ">> migrate done" | tee -a $LOGFILE
+
+python manage.py createcachetable | tee -a $LOGFILE
+echo ">> createcachetable done" | tee -a $LOGFILE
+
+python manage.py collectstatic --noinput | tee -a $LOGFILE
+echo ">> collectstatic done" | tee -a $LOGFILE
+
+if python manage.py showmigrations | grep -q data; then
+  python manage.py loaddata data
+else
+  echo ">> no data fixture, skipping loaddata"
+fi
+
+
 # Start Cron
 # service cron start
 
 # Start Django server
-python3 manage.py runserver 0.0.0.0:8042 >> /app/be-kowl.log 2>&1
+# python3 manage.py runserver 0.0.0.0:8042 >> /app/be-kowl.log 2>&1
+
+echo "✅ Django setup complete. Starting Uvicorn..." | tee -a $LOGFILE
+
+exec uvicorn kwl.asgi:application --host 0.0.0.0 --port 8042
