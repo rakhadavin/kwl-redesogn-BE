6cb633afcc6a68397f3aae16922203b154d10cd8
adam.muhammad <adam.muhammad@cs.ui.ac.id>
2026-03-10 13:29:07 +0700

    fix: Refactor KwlPoint retrieval to use filter and handle creation logic more explicitly

--- wtk/views.py
+++ wtk/views.py
@@ -216,7 +216,12 @@ class WtkMultipleVoteView(APIView):
                 wtk_poll_question = WtkPollQuestion.objects.get(wtk__topic_id=topic)
                 student_answer, answer_created = WtkPollStudentAnswer.objects.get_or_create(wtk_poll=wtk_poll_question, student=student)
 
-                kwl_point, kwl_created = KwlPoint.objects.get_or_create(student=student, topic=wtk_poll_question.wtk.topic)
+                kwl_point = KwlPoint.objects.filter(student=student, topic=wtk_poll_question.wtk.topic).first()
+                if kwl_point is None:
+                    kwl_point = KwlPoint.objects.create(student=student, topic=wtk_poll_question.wtk.topic)
+                    kwl_created = True
+                else:
+                    kwl_created = False
                 reward_point, reward_created = RewardStudentPoint.objects.get_or_create(student=student, course=wtk_poll_question.wtk.topic.course)
                 
                 student_answer.choices.clear()
@@ -268,7 +273,12 @@ class WtkEssayAnswerView(APIView):
                 answer.save()
 
                 student_point, reward_created = RewardStudentPoint.objects.get_or_create(student=student, course=wtk_reflection.wtk.topic.course)
-                kwl_point, kwl_created = KwlPoint.objects.get_or_create(student=student, topic=wtk_reflection.wtk.topic)
+                kwl_point = KwlPoint.objects.filter(student=student, topic=wtk_reflection.wtk.topic).first()
+                if kwl_point is None:
+                    kwl_point = KwlPoint.objects.create(student=student, topic=wtk_reflection.wtk.topic)
+                    kwl_created = True
+                else:
+                    kwl_created = False
                 
                 kwl_point.kwl_status = 'wtk'
                 kwl_point.wtk_score = wtk_reflection.score
